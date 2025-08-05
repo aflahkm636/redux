906c9ae1efc5e30dcb1f785e8bbc4f3c8fdd283f
aflahkm636 <aflahkm007@gmail.com>
2025-08-05 11:09:05 +0530

    counterReducer

--- src/components/middleWare/CounterStore.jsx
+++ src/components/middleWare/CounterStore.jsx
@@ -1,8 +1,20 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import { CounterReducer } from "./CounterReducer";
 
-const store = createStore(CounterReducer,
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+// Logger middleware
+const Logger = (store) => (next) => (action) => {
+    console.log("dispatching", action);
+    const result = next(action);
+    console.log("next state", store.getState());
+    return result;
+};
+
+// Use Redux DevTools compose if available
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    CounterReducer,
+    composeEnhancers(applyMiddleware(Logger))
+);
 
 export default store;
